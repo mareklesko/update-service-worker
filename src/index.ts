515c6d0586adfb1a3f39571e61787989e5696aeb
Marek Lesko <mlesko100@msn.com>
2020-09-18 11:18:00 +0200

    ADDED description field.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,44 @@
 #!/usr/bin/env node
 
-
 const jsonfile = require('jsonfile')
 const program = require('commander');
-
+const readline = require("readline");
 
 
 class Replacer {
     private version: any;
     private ngsw: any;
-    constructor(private packageFile: string, private ngswConfigFile: string) { }
+    private rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+    constructor(private packageFile: string, private ngswConfigFile: string, private description: string) {
+        if (!description) {
+            this.rl.question("Description: ", (d: any) => {
+                this.description = d;
+                this.rl.close();
+            });
+            this.rl.on('close', () => this.run());
+        }
+        else {
+            this.run();
+        }
+    }
+
 
     run() {
         this.version = jsonfile.readFileSync(this.packageFile).version;
-        this.ngsw = jsonfile.readFileSync(this.ngswConfigFile);
-        this.ngsw.appData = this.ngsw.appData ? { ...this.ngsw.appData, version: this.version } : { version: this.version };
-        jsonfile.writeFileSync(this.ngswConfigFile, this.ngsw, { flag: 'w' });
+        jsonfile.readFile(this.ngswConfigFile)
+            .then((d: any) => {
+                this.ngsw = d || {};
+                this.ngsw.appData = this.ngsw.appData ? { ...this.ngsw.appData, version: this.version } : { version: this.version };
+                this.ngsw.appData.description = this.description;
+                jsonfile.writeFile(this.ngswConfigFile, this.ngsw, { flag: 'w' })
+                    .then((d: any) => {
+                        console.log(`${this.ngswConfigFile} successfully updated.`);
+                        process.exit();
+                    })
+                    .catch((d: any) => console.log(d))
+            })
+            .catch((d: any) => console.log(d));
     }
 }
 
@@ -26,11 +49,11 @@ program
     .description("CLI to update ngsw-config.json with version from package.json")
     .option('-p, --package <package.json>', 'package.json file path')
     .option('-n, --ngsw <ngsw-config.json>', 'ngsw-config.json file path')
+    .option('-d --desc <update description>', 'description of the update')
     .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
     program.outputHelp();
 } else {
-    var r = new Replacer(program.package, program.ngsw);
-    r.run();
+    new Replacer(program.package, program.ngsw, program.desc);
 }
